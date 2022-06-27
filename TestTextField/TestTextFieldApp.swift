c4c9258aedb4588823ec168f4ab15dbd44d3c966
Mohamed Alwakil <mohamed.diaa@qvik.fi>
2022-06-27 11:19:41 +0200

    Initial Commit

---
+++ TestTextField/TestTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTextFieldApp.swift
+//  TestTextField
+//
+//  Created by Mohamed Alwakil on 2022-06-27.
+//
+
+import SwiftUI
+
+@main
+struct TestTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
